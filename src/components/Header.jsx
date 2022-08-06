494dc5f53858335bdb473bccfa7e77328a29aeff
Nooruz <rrisovanie638@gmail.com>
2022-08-06 12:30:01 +0600

    created cart component layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import mealsImage from '../img/meals.jpeg'
 import classes from './Header.module.css'
+import { HeaderCartButton } from './Layout/HeaderCartButton'
 
 export const Header = () => {
     return (
-     <>
-    <header className={classes.header}>
-        <h1>React Meals</h1>
-        <button>Cart</button>
-    </header>
-    <div className={classes['main-image']}>
-        <img src={mealsImage} alt='food'/>
-    </div>
-    </>
-    )
+      <>
+        <header className={classes.header}>
+          <h1>React Meals</h1>
+          <HeaderCartButton />
+        </header>
+        <div className={classes["main-image"]}>
+          <img src={mealsImage} alt="food" />
+        </div>
+      </>
+    );
 }
